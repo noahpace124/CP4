7029ad47dad04a254cb749d5d6586271bdac15de
gquigley1313 <108026669+gquigley1313@users.noreply.github.com>
2022-12-07 15:36:46 -0800

    Update animals.js

--- src/back-end/animals.js
+++ src/back-end/animals.js
@@ -7,6 +7,42 @@ const animals = [
         habitat: 'Land',
         diet: 'Carnivorous'
     }
+},
+{
+    name: 'Dog',
+    desc: {
+        height: '2\'3\"',
+        weight: '34 lbs',
+        habitat: 'Land',
+        diet: 'Omnivorous'
+    }
+},
+{
+    name: 'Whale',
+    desc: {
+        height: '93\'0\"',
+        weight: '3500 lbs',
+        habitat: 'Sea',
+        diet: 'Carnivorous'
+    }
+},
+{
+    name: 'Shark',
+    desc: {
+        height: '16\'0\"',
+        weight: '2400 lbs',
+        habitat: 'Sea',
+        diet: 'Carnivorous'
+    }
+},
+    {
+    name: 'Tiger',
+    desc: {
+        height: '3\'2\"',
+        weight: '350 lbs',
+        habitat: 'Land',
+        diet: 'Carnivorous'
+    }
 }
 ];
 
