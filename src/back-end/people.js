894547c8ab49abdebbd94aabadccd3ed822e223a
gquigley1313 <108026669+gquigley1313@users.noreply.github.com>
2022-12-07 15:29:34 -0800

    Update people.js

--- src/back-end/people.js
+++ src/back-end/people.js
@@ -9,6 +9,97 @@ const people = [
         animal: 'Shark'
     }
 }
+{
+    name: 'Carole Baskin',
+    age: 61,
+    desc: {
+        height: '5\'4\"',
+        weight: '165 lbs',
+        color: 'Blue',
+        animal: 'Tiger'
+    }
+}
+{
+    name: 'Taylor Swift',
+    age: 32,
+    desc: {
+        height: '5\'11\"',
+        weight: '119 lbs',
+        color: 'Purple',
+        animal: 'Cat'
+    }
+}
+{
+    name: 'Shaggy Rogers',
+    age: 17,
+    desc: {
+        height: '6\'3\"',
+        weight: '125 lbs',
+        color: 'Green',
+        animal: 'Dog'
+    }
+}
+{
+    name: 'Captain Ahab',
+    age: 58,
+    desc: {
+        height: '6\'0\"',
+        weight: '180 lbs',
+        color: 'Blue',
+        animal: 'Whale'
+    }
+}
+{
+    name: 'John Cena',
+    age: 45,
+    desc: {
+        height: '6\'1\"',
+        weight: '251 lbs',
+        color: 'Green',
+        animal: 'Tiger'
+    }
+}
+{
+    name: 'Jimmy Fallon',
+    age: 48,
+    desc: {
+        height: '6\'0\"',
+        weight: '160 lbs',
+        color: 'Blue',
+        animal: 'Dog'
+    }
+}
+{
+    name: 'Kanye West',
+    age: 45,
+    desc: {
+        height: '5\'8\"',
+        weight: '163 lbs',
+        color: 'Black',
+        animal: 'Shark'
+    }
+}
+{
+    name: 'Barack Obama',
+    age: 61,
+    desc: {
+        height: '6\'2\"',
+        weight: '179 lbs',
+        color: 'White',
+        animal: 'Cat'
+    }
+}
+{
+    name: 'Buddy The Elf',
+    age: 30,
+    desc: {
+        height: '6\'3\"',
+        weight: '218 lbs',
+        color: 'Yellow',
+        animal: 'Whale'
+    }
+}
+
 ];
 
 module.exports = people;
